ef7e25891451b9b984154d9518c3c21dd72daefc
Maria Claudia <mariacladiaperezes@gmail.com>
2021-03-25 19:29:23 -0300

    se agrego la interface de movie

--- src/app/disneyPlus/components/recommended/recommended.component.ts
+++ src/app/disneyPlus/components/recommended/recommended.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
 
 @Component({
   selector: 'app-recommended',
@@ -6,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recommended.component.css']
 })
 export class RecommendedComponent implements OnInit {
+  config: SwiperConfigInterface = {
+    slidesPerView: 5,
+    spaceBetween: 20,
+    slidesPerGroup: 4,
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  };
 
   constructor() { }
 
--- src/app/disneyPlus/services/disney.service.ts
+++ src/app/disneyPlus/services/disney.service.ts
@@ -5,5 +5,8 @@ import { Injectable } from '@angular/core';
 })
 export class DisneyService {
 
+  private apiUrl = 'https://api.themoviedb.org/3';
+  private apiKey = 'api_key=d20775fff92244e778e6196b31f18576';
+
   constructor() { }
 }
