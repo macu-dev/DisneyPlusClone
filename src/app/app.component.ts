f4b08fc36fa4014289862764d5bc9ff415e7fe3b
Maria Claudia <mariacladiaperezes@gmail.com>
2021-05-24 01:03:45 -0300

    se agrego el preload a la pagina principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,5 @@
+import { Renderer2 } from '@angular/core';
+import { AfterViewInit } from '@angular/core';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,6 +7,12 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'disney-plus-app';
+  constructor(private renderer: Renderer2) {}
+
+  ngAfterViewInit(): void {
+    const loader = this.renderer.selectRootElement('#loader');
+    this.renderer.setStyle(loader, 'display', 'none');
+  }
 }
